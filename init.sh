f00298e251588a18f2a01ba65843110d26d0e22b
Rahul Prasad <therahulprasad@users.noreply.github.com>
2019-01-28 14:12:16 +0530

    install z and make rerunnable
    You can rerun this script without overwriting anything
    Byobu configuration fixed

--- init.sh
+++ init.sh
@@ -4,7 +4,10 @@ sudo chsh -s /bin/zsh $USER
 # Install Oh My zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # Fuzzy finder from https://github.com/junegunn/fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --key-bindings --completion --update-rc
+ls $HOME/.fzf || (git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && $HOME/.fzf/install --key-bindings --completion --update-rc)
 # Configure zsh in byobu
-grep /bin/zsh ~/.byobu/.tmux.conf || (echo "set -g default-shell /bin/zsh" >> ~/.byobu/.tmux.conf; echo "set -g default-command /bin/zsh" >> ~/.byobu/.tmux.conf)
+ls $HOME/.byobu/ || mkdir $HOME/.byobu
+grep /bin/zsh $HOME/.byobu/.tmux.conf || (echo "set -g default-shell /bin/zsh" >> $HOME/.byobu/.tmux.conf; echo "set -g default-command /bin/zsh" >> $HOME/.byobu/.tmux.conf)
+# Configure z
+ls $HOME/z.sh || (wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O $HOME/z.sh && chmod +x $HOME/z.sh)
+grep z.sh $HOME/.zshrc || echo ". $HOME/z.sh" >> $HOME/.zshrc
